3af9cceefd9359b210a3c5762f69fd85c21dbcf5
alex <greuteralexander@gmail.com>
2020-01-03 19:40:38 +0100

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {WebService, Country} from './shared/web.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'currency2';
+  countries: Array<Country> = new Array<Country>();
+
+  selected1: Country = undefined;
+  selected2: Country = undefined;
+
+  amount1 = 0;
+
+  loading = true;
+
+  constructor(private webService: WebService) {
+  }
+
+  ngOnInit(): void {
+    this.webService.getCountries().subscribe(data => {
+      for (const key in data) {
+        this.countries.push({key: key.toString(), viewValue: data[key]});
+      }
+      this.loading = false;
+      this.selected1 = this.countries[0];
+      this.selected2 = this.countries[1];
+    });
+  }
+
+  convertedAmount(): number {
+    return this.webService.convert(this.amount1, this.selected1, this.selected2);
+  }
+
+  swapCountries() {
+    const previous = this.selected1;
+    this.selected1 = this.selected2;
+    this.selected2 = previous;
+  }
 }
